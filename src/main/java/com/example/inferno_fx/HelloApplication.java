52e0503b36dffdf92090846994d7d4c9f5d04f0c
Parsa Bagheri <107321532+parsssa@users.noreply.github.com>
2022-06-16 14:49:59 +0200

    Creato file fxml da aprire con scene builder

--- src/main/java/com/example/inferno_fx/HelloApplication.java
+++ src/main/java/com/example/inferno_fx/HelloApplication.java
@@ -2,6 +2,7 @@ package com.example.inferno_fx;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,10 +11,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
+        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        stage.setTitle("FireNews24");
+        stage.setScene(new Scene(root));
         stage.show();
     }
 
