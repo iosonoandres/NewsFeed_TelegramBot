5b4cba1f32446aa25c3b499a1f9c33407e23d891
Parsa Bagheri <107321532+parsssa@users.noreply.github.com>
2022-06-21 14:11:20 +0200

    cambiato ordine di Username e Password che prima era sbagliato sul file .fxml

---
+++ src/main/java/com/example/inferno_fx/MapSample/TestMapSample.java
@@ -0,0 +1,12 @@
+package com.example.inferno_fx.MapSample;
+
+public class TestMapSample {
+
+
+    public static void main(String[] args) {
+        MapSample ms1 = new MapSample();
+        ms1.inizializza();
+        System.out.println(ms1);
+
+    }
+}
